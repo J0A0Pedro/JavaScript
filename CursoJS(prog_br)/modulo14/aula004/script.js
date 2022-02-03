0d9d35eeb2f5fe5d3bef8ee4dd18a0d4cc6fec3a
João Pedro <pedroraimundodossantos13@gmail.com>
2022-02-03 19:48:57 -0300

    nova_aula

---
+++ CursoJS(prog_br)/modulo14/aula004/script.js
@@ -0,0 +1,58 @@
+
+/*function dobro (x){
+    console.log(2 * x);
+}
+dobro(5);
+//10
+*/
+
+/*
+let dobro = function(x){
+    console.log(2 * x);
+}
+dobro(2);
+//4
+*/
+
+
+let dobro = (x) => {
+    console.log(2 * x)
+}
+dobro(10);
+//20
+
+ 
+let metade = x => {
+    return ( x / 2);
+}
+console.log(metade(10));
+//5
+
+
+
+let metade2 = x => ( x / 2);
+
+console.log(metade2(10));
+//5
+
+
+
+
+
+
+/*let triplo = () => console.log(this);*/
+
+
+let triplo = function (){
+    return 3 * this.x;
+}
+
+let numero = {
+    x: 8, 
+    d: triplo
+}
+
+numero.d();
+
+console.log(numero.d());
+//24
