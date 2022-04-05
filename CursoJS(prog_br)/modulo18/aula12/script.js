635f59459d18f781ba9d5c1d0aea99fe56f42663
João Pedro <pedroraimundodossantos13@gmail.com>
2022-04-05 20:25:21 -0300

    aula12

---
+++ CursoJS(prog_br)/modulo18/aula12/script.js
@@ -0,0 +1,21 @@
+
+
+// $("#lista1").click( ()=>{
+//     $("#lista1").remove();
+// });
+
+//removemos a lista1 toda
+
+//REMOVENDO MAIS DE UM ITEM AO MESMO TEMPO:  
+$("li").remove(".item2, .item3, .item5")
+
+//Mas e se eu nao quisesse remover a lista e só quisesse remover todos os itens da lista?
+
+//Método Empty():
+
+
+// $("#lista1").click( ()=>{
+//     $("#lista1").empty();
+// });
+
+//aqui nós ainda temos a lista, mas ela está vazia
