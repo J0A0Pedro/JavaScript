aab09d19d67f977350574cbbb3cca0985e82eda0
João Pedro <pedroraimundodossantos13@gmail.com>
2022-06-01 23:39:45 -0300

    Update index.js

--- CursoJS(prog_br)/modulo21/aula006/index.js
+++ CursoJS(prog_br)/modulo21/aula006/index.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-    console.log(req);
-    res.send("<h1>Hello World Hoje</h1>")
-});
+//para qualquer tipo de chamada nesse caminho eu quero imprimir qual é o corpo da minha requisição
+let consoleBody = (req, res, next) => {
+    console.log(req.body);
+    next(); 
+}
+ 
+let helloWorld = (req, res) => {
+    res.send("Hello World!!!!")
+}
+// app.get("/", consoleMethod, helloWorld);
+// app.post("/", consoleMethod, helloWorld);
+//separamos eles: 
+
+app.use("/", bodyParser.json())
+app.use("/", consoleBody);
+app.get("/", helloWorld);
+app.post("/", helloWorld)
+
+
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
